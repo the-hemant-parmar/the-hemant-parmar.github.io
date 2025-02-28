ba97e9551ba487b550f8599651f9345ee126510c
the-hemant-parmar <gamjkjamoniya@gmail.com>
2025-02-28 13:40:06 +0530

    Activate email service

--- send-email.js
+++ send-email.js
@@ -6,16 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		e.preventDefault();
 
 		// Replace 'YOUR_SERVICE_ID' and 'YOUR_TEMPLATE_ID' with your EmailJS Service and Template IDs.
-		emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form).then(
-			function (response) {
-				console.log("SUCCESS!", response.status, response.text);
-				alert("Message sent successfully!");
-				form.reset(); // Optionally reset the form after successful submission
-			},
-			function (error) {
-				console.error("FAILED...", error);
-				alert("Failed to send message. Please try again later.");
-			}
-		);
+		emailjs
+			.sendForm("agyat_contact_service", "agyat_contact_form", form)
+			.then(
+				function (response) {
+					console.log("SUCCESS!", response.status, response.text);
+					alert("Message sent successfully!");
+					form.reset();
+				},
+				function (error) {
+					console.error("FAILED...", error);
+					alert("Failed to send message. Please try again later.");
+				}
+			);
 	});
 });
